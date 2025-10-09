16272af91131325d557d65346c561daebfe1a990
Ángel Trotter Padrón <atrotterpadron@gmail.com>
2025-10-10 00:32:58 +0200

    commit

--- The_Bear_Madrid/src/app/interceptors/authe.interceptor.ts
+++ The_Bear_Madrid/src/app/interceptors/authe.interceptor.ts
@@ -8,59 +8,60 @@ import { RestClienteService } from '../servicios/rest-cliente.service';
 import IRestMessage from '../modelos/IRestMessage';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const _storageSvc=inject(HTTP_INJECTIONTOKEN_STORAGE_SVCS);
-  const _restCliente=inject(RestClienteService);
+  const _storageSvc = inject(HTTP_INJECTIONTOKEN_STORAGE_SVCS);
+  const _restCliente = inject(RestClienteService);
 
-  const _jwts:IJwt =_storageSvc.getJWT();
-  
-  let _authreq=req;
+  const _jwts: IJwt = _storageSvc.getJWT();
+
+  let _authreq = req;
   console.log('objeto peticion antes de clonar ', req);
 
-  if( _jwts.sesion !== '' || _jwts.verificacion !=='' ){
+  if (_jwts.sesion !== '' || _jwts.verificacion !== '') {
 
-      _authreq=req.clone(
+    _authreq = req.clone(
       {
-        headers: req.headers.append('Authorization', `Bearer ${ _jwts.sesion !=='' ? _jwts.sesion: _jwts.verificacion }`)
+        headers: req.headers.append('Authorization', `Bearer ${_jwts.sesion !== '' ? _jwts.sesion : _jwts.verificacion}`)
       }
-     );
-     
-     console.log('objeto peticion despues de clonar ', _authreq);     
-    } 
-
-    return next(_authreq).pipe(
-      tap( (evento:HttpEvent<any>)=> console.log('valor del observable', evento)),
-      catchError( (error:any) => {
-                                    console.log('error en respueta en observable peticion request saliente... ', error);
-                                    if(error.status==403) {
-                                    
-                                      const _refresh=_jwts?.refresh;
-                                      if(! _refresh) return throwError(()=> new Error('no existe refreshtoken'))//_router.navigateByUrl('/Cliente/Login');
-
-                                      _restCliente.RefrescarTokens(_refresh).pipe(
-                                          tap( (resp:IRestMessage) => console.log('evento interceptado en interceptor en observable peticion RefrescarTokens: ', resp) ),
-                                          map( (resp:IRestMessage) => {
-                                                  if (resp.codigo == 0 ){
-                                                    //tengo nuevos jwt de sesion y refresh, mando peticion original del cliente con nuevo jwt-sesion
-                                                    let _retryreq=req.clone(
-                                                      {
-                                                        setHeaders: { Authorization: `Bearer ${resp.datos!.sesion}`}
-                                                      }
-                                                    );
-                                                    return next(_retryreq);
-                                                    
-                                                  } else {
-                                                    //ha ido mal la generacion de nuevos tokens...al login directamente...
-                                                    return throwError(()=> new Error('fallo en regeneracion de token de sesion a partir del refresh-token'))//_router.navigateByUrl('/Cliente/Login');
-                                                  }
-                                          })
-                                          
-                                        )
-                                      
-                                      }
-                                    
-                                    return throwError( ()=> new Error('error recibido desde nodejs...' + error) )  
-
-                                  }
-                                )
     );
+
+    console.log('objeto peticion despues de clonar ', _authreq);
+  }
+
+  return next(_authreq).pipe(
+    tap((evento: HttpEvent<any>) => console.log('valor del observable capturado por el interceptor...', evento)),
+    catchError((error: any) => {
+      console.log('error en respueta en observable peticion request saliente... ', error);
+      
+      if (error.status == 403) {
+
+        const _refresh = _jwts?.refresh;
+        if (!_refresh) return throwError(() => new Error('no existe refreshtoken'))//_router.navigateByUrl('/Cliente/Login');
+
+        _restCliente.RefrescarTokens(_refresh).pipe(
+          tap((resp: IRestMessage) => console.log('evento interceptado en interceptor en observable peticion RefrescarTokens: ', resp)),
+          map((resp: IRestMessage) => {
+            if (resp.codigo == 0) {
+              //tengo nuevos jwt de sesion y refresh, mando peticion original del cliente con nuevo jwt-sesion
+              let _retryreq = req.clone(
+                {
+                  setHeaders: { Authorization: `Bearer ${resp.datos!.sesion}` }
+                }
+              );
+              return next(_retryreq);
+
+            } else {
+              //ha ido mal la generacion de nuevos tokens...al login directamente...
+              return throwError(() => new Error('fallo en regeneracion de token de sesion a partir del refresh-token'))//_router.navigateByUrl('/Cliente/Login');
+            }
+          })
+
+        )
+
+      }
+
+      return throwError(() => new Error('error recibido desde nodejs...' + error))
+
+    }
+    )
+  );
 };

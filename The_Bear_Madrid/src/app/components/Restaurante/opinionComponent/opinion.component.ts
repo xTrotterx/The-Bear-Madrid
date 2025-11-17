89aca65507b3848cf334a46a58a1354c7fb2fd1d
Ángel Trotter Padrón <atrotterpadron@gmail.com>
2025-11-17 13:39:12 +0100

    visuals

--- The_Bear_Madrid/src/app/components/Restaurante/opinionComponent/opinion.component.ts
+++ The_Bear_Madrid/src/app/components/Restaurante/opinionComponent/opinion.component.ts
@@ -1,14 +1,20 @@
 import { Component, Input } from '@angular/core';
 import IOpinion from '../../../modelos/Interfaces/IOpinion';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-opinion-component',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './opinion.component.html',
   styleUrl: './opinion.component.css'
 })
 export class OpinionComponent {
   
   @Input() opinion!: IOpinion;
-
+public getPuntuacionColor(puntuacion: number): string {
+    if (puntuacion >= 4) return '#28a745'; // Verde - Excelente
+    if (puntuacion >= 3) return '#ffc107'; // Amarillo - Bueno
+    if (puntuacion >= 2) return '#fd7e14'; // Naranja - Regular
+    return '#dc3545'; // Rojo - Malo
+  }
 }
